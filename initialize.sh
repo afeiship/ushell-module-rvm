1f920b783b23032e04847bfc96f19cd4b841a5b9
afeiship <1290657123@qq.com>
2018-09-04 10:22:52 +0800

    issue: udapte scripts

--- initialize.sh
+++ initialize.sh
@@ -4,5 +4,7 @@ ROOT_PATH=$(dirname $BASH_SOURCE);
 
 # gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
 # \curl -sSL https://get.rvm.io | bash -s stable
+# source /home/ubuntu/.rvm/scripts/rvm
+# install ruby: rvn install ruby-2.3.7
 
 unset ROOT_PATH;
